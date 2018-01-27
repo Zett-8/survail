dc7234fcd4d5ec8841b367e8e8945d5a66a90b1b
toshiki kamei <toshiki@toshikis-MBPr.local>
2018-01-27 16:34:14 +0900

    chage index

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>hello world</h1>');
+  res.send({
+    store: {
+      simona: 'hello',
+      toshiki: 'world'
+    }});
 });
 
 const PORT = process.env.PORT || 5000;
