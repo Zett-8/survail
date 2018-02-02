244683ae7d23fa9d3599902f13b8de2fa43ba054
toshiki kamei <toshiki@toshikis-MBPr.local>
2018-02-02 19:46:40 +0900

    finished auth flow

---
+++ services/passport.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id }).then(existingUser => {
+      if (existingUser) {
+        console.log('already exist');
+        done(null, existingUser); // if you don't have an error, put null as first argument
+      } else {
+        new User({ googleId: profile.id }).save().then(user => done(null, user));
+      }
+    })
+  })
+);
