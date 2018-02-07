c0a7dc397d6a62a4d34795e5fdf9562673b0b73e
toshiki kamei <toshiki@toshikis-MBPr.local>
2018-02-07 09:40:22 +0900

    fix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,10 +5,6 @@ module.exports = app => {
     scope: ['profile', 'email']
   }));
 
-  app.get('/', (req, res) => {
-    res.send('<h1>Hello this is inside of the server :5000</h1>');
-  });
-
   app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => res.redirect('/surveys'));
 
 
